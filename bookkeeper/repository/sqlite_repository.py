b1410e681b1d3f053cab8306b68f28861264ff17
Gressy2113 <veretenenko.ii@phystech.edu>
2023-03-05 18:00:25 +0300

    Implemented the add() method for SQL repository

---
+++ bookkeeper/repository/sqlite_repository.py
@@ -0,0 +1,52 @@
+import sqlite3
+from inspect import get_annotations
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+
+
+class SQLiteRepository(AbstractRepository[T]):
+    def __init__(self, db_file: str, cls: type) -> None:  # TODO: создание таблиц, если они не существуют
+        self.db_file = db_file
+        self.table_name = cls.__name__.lower()
+        self.fields = get_annotations(cls, eval_str=True)
+        self.fields.pop('pk')
+
+    
+    def add(self, obj: T) -> int:
+        names = ", ".join(self.fields.keys())
+        p = ", ".join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES ({p})', values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
+
+
+    def get(self, pk: int) -> T | None:
+        """ Получить объект по id """
+        pass
+
+    def get_all(self, where: dict[str, any] | None = None) -> list[T]:
+        """
+        Получить все записи по некоторому условию
+        where - условие в виде словаря {'название_поля': значение}
+        если условие не задано (по умолчанию), вернуть все записи
+        """
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(f'SELECT * FROM {self.table_name}')  # TODO: добавить блок WHERE
+            res = cur.fetchall()
+        con.close()
+        return res
+
+    def update(self, obj: T) -> None:
+        """ Обновить данные об объекте. Объект должен содержать поле pk. """
+        pass
+
+    def delete(self, pk: int) -> None:
+        """ Удалить запись """        
+        pass
